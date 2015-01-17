f6bca8ed2e59f89f8244182730c93fbdfaadc361
danisio <danisio83@gmail.com>
2015-01-17 14:03:02 +0200

    IntroductionToProgramming homework

---
+++ IntroductionToProgramming/07.PrintFirstAndLastName/FirstAndLastName.cs
@@ -0,0 +1,17 @@
+﻿/*Problem 7. Print First and Last Name
+Create console application that prints your first and last name, each at a separate line.
+ */
+
+using System;
+
+class FirstAndLastName
+{
+    static void Main()
+    {
+        string firstName = "Boiko";
+        string lastName = "Borisov";
+        Console.WriteLine("My full name is:");
+        Console.WriteLine(firstName);
+        Console.WriteLine(lastName);
+    }
+}
---
+++ IntroductionToProgramming/15.AgeAfter10Years/AgeAfter10Years.cs
@@ -0,0 +1,22 @@
+﻿/*Problem 15.* Age after 10 Years
+Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+ */
+ 
+using System;
+
+class AgeAfter10Years
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your birthdate in format dd.mm.yyyy");
+            DateTime dateBirth = DateTime.Parse(Console.ReadLine());
+            DateTime dateToday = DateTime.Now;
+            int age = dateToday.Year - dateBirth.Year;
+
+            if (dateToday.Month < dateBirth.Month || (dateToday.Month == dateBirth.Month && dateToday.Day < dateBirth.Day))
+            {
+                age--;
+            }
+            Console.WriteLine("You are {0} years old. In 10 years you'll be {1}.", age, age + 10);
+        }
+    }
